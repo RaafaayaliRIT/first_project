6b5a3e3883b0d376ac3d194cca60615f6d47ac0d
RaafaayaliRIT <rsa1184@g.rit.edu>
2025-04-14 01:20:01 -0400

    relearning java again

---
+++ HelloWorld.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+public class HelloWorld {
+
+
+     private String name;
+     private int age;
+
+     public HelloWorld(String input){
+          this.name = input;
+          this.age = generateAge();
+     }
+
+     private int generateAge(){
+          Random RNG = new Random();
+          int age = RNG.nextInt(0,11);
+          return age;
+
+     }
+
+     public String getName(){return this.name;}
+     public int getAge(){return this.age;}
+     
+
+
+     
+     public static void main(String[] args) {
+          System.err.println("hello word");
+
+          HelloWorld world = new HelloWorld("Raafay");
+          System.out.println(world.getAge());
+          System.out.println(world.getAge());
+
+     }
+}
